0ba78ec649bd3683f28d5700abe32d303fd14998
unknown <Owner@.(none)>
2010-06-08 13:20:23 -0400

    Worked on createbot code a bit.

--- createbot/config.lua
+++ createbot/config.lua
@@ -1,5 +1,12 @@
  local servoutils = require "mb.servoutils"
+ local drivemod = require "mb.drive"
  
  grip_servo = servoutils.SpeedControlServo{1}
  updown_servo = servoutils.SpeedControlServo{2}
  
+ drivetrain = drivemod.CreateDriveTrain{}
+ drive = drivemod.Drive{
+	drivetrain = drivetrain,
+	style = drivemod.Smooth{accel=20},
+	topvel = 19
+}
