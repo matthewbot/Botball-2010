9089b493f11b2b285e6d94023da3d8e871c5d4df
Matthew Thompson <matthewbot@gmail.com>
2010-06-08 22:28:17 -0400

    Indentation fixes, small claw testing function

--- createbot/config.lua
+++ createbot/config.lua
@@ -1,11 +1,11 @@
- local servoutils = require "mb.servoutils"
- local drivemod = require "mb.drive"
+local servoutils = require "mb.servoutils"
+local drivemod = require "mb.drive"
  
- grip_servo = servoutils.SpeedControlServo{1}
- updown_servo = servoutils.SpeedControlServo{2}
+grip_servo = servoutils.SpeedControlServo{1}
+updown_servo = servoutils.SpeedControlServo{2}
  
- drivetrain = drivemod.CreateDriveTrain{}
- drive = drivemod.Drive{
+drivetrain = drivemod.CreateDriveTrain{}
+drive = drivemod.Drive{
 	drivetrain = drivetrain,
 	style = drivemod.Smooth{accel=20},
 	topvel = 19
--- createbot/interact.lua
+++ createbot/interact.lua
@@ -1,2 +1,14 @@
 import "config"
 create = require "mb.create"
+
+function dumptest()
+	claw.close(); 
+	task.sleep(1);
+	claw.up{speed="full"}
+	task.sleep(1);
+	claw.release_basket();
+	task.sleep(1);
+	claw.down{wait=true}
+	claw.open()
+end
+
