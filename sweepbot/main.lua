834aa782a18ed20838e97894e949b14140e2e502
unknown <Owner@.(none)>
2010-06-14 13:14:45 -0400

    worked on main

--- sweepbot/main.lua
+++ sweepbot/main.lua
@@ -16,24 +16,24 @@ end
 function main()
 	task.async(init)
 	drive:bk{inches=27}
-	drive:rturn{degrees=90, speed=400}
+	drive:rturn{degrees=90}
 	drive:bk{inches=32}
-	drive:bk{inches=3, speed=400}
+	drive:bk{inches=3}
 	drive:fd{inches=4.5}
-	drive:lturn{degrees=90, speed=400}
+	drive:lturn{degrees=90}
 	drive:fd{inches=13}
 	drive:off()
 	algorithms.wall_lineup_bumpers()
-	drive:bk{inches=1.25, speed=400}
+	drive:bk{inches=1.25}
 	arm.extend(550, 400)
-	drive:lturn{degrees=90, speed=400}
-	drive:bk{inches=6, speed=400}
-	drive:fd{inches=1, speed=400}
+	drive:lturn{degrees=90}
+	drive:bk{inches=6}
+	drive:fd{inches=1}
 	drive:scooch{xdist=2, speed=300}
 	drive:fd{inches=10}
-	drive:lturn{degrees=40, speed=400}
+	drive:lturn{degrees=40}
 	arm.extend(400, 400)
-	drive:bk{inches=3, speed=400}
+	drive:bk{inches=3}
 	sweep.palms()
 --[[	algorithms.follow_wall_sensor()
 	algorithms.final_palm_lineup()
