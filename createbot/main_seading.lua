924856c176a8db5b248022dc9bc9eda4fcde1c5d
Matt Thompson <matthewbot@gmail.com>
2010-06-29 21:12:48 -0400

    Worked on createbot seeding program

--- createbot/main_seading.lua
+++ createbot/main_seading.lua
@@ -12,21 +12,113 @@ function main()
 	
 	grab_pile(20)
 	grab_pile(23)
-	grab_pile(21)
-	drive:fd{inches=11} -- wall lineup
+	grab_pile(23)
+	drive:fd{inches=9} -- wall lineup
 	drive:fd{vel=8, time=1}
 	
 	drop_sponge(-42, "medium")
 	drop_sponge(15, "small")
+	
+	drive:fd{inches=15} -- wall lineup
+	drive:fd{vel=8, time=1}
+	drive:bk{inches=4}
+	drive:rturn{degrees=90}
+	drive:bk{vel=8, wait=create.bump}
+	
+	grab_pile(20)
+	
+	drop_sponge(-5, "large")
+	
+	drive:fd{inches=20} -- wall lineup
+	drive:fd{vel=8, time=1}
+	drive:bk{inches=4}
+	drive:rturn{degrees=85}
+	
+	drive:fd{inches=33} -- line up against duck scoring area
+	drive:fd{vel=8, time=1}
+	drive:bk{inches=1}
+	drive:lturn{degrees=75}
+	claw.down{wait=true}
+	claw.eject()
+	
+	claw.up()
+	drive:bk{inches=10}
+	drive:rturn{degrees=75}
+	drive:fd{inches=37}
+	drive:fd{vel=8, time=1}
+	
+	clean_ducks()
+end
+
+function clean_ducks()
+	drive:bk{inches=11}
+	drive:lturn{degrees=90}
+	drive:fd{vel=8, time=1}
+	drive:bk{inches=2.5}
+	
+	task.async(function ()
+		task.sleep(.3)
+		grip_servo(1400)
+	end)
+	claw.down_grab{wait=true}
+	claw.close()
+	task.sleep(.5)
+	claw.lift{wait=true}
+	drive:bk{inches=14}
+	drive:bk{vel=8, wait=create.bump}
+	drive:lturn{degrees=95}
+	drive:bk{inches=7.25}
+	claw.down{wait=true}
+	claw.open{speed=700, wait=true}
+	task.sleep(.5)
+	
+	task.async(function ()
+		task.sleep(.3)
+		claw.up()
+		task.sleep(.4)
+		claw.close()
+	end)
+	drive:bk{vel=8, wait=create.bump}
+	drive:rturn{degrees=45}
+	drive:fd{inches=12}
+	drive:rturn{degrees=45}
+	drive:fd{inches=10}
+	drive:fd{vel=8, time=1}
+	
+	task.async(function ()
+		task.sleep(.3)
+		claw.open()
+	end)
+	claw.down_grab{wait=true}
+	claw.close()
+	task.sleep(.5)
+	claw.lift{wait=true}
+	
+	drive:bk{inches=10}
+	drive:lturn{degrees=90}
+	drive:bk{inches=7.25}
+	claw.down{wait=true}
+	claw.open{speed=700, wait=true}
+	drive:bk{vel=8, wait=create.bump}
+	
+	claw.up()
+	task.sleep(.4)
+	claw.close()
+	drive:lturn{degrees=90}
+	drive:fd{inches=10}
+	drive:fd{vel=8, time=1}
 end
 	
-function grab_pile(fddist)
+function grab_pile(fddist, chargedelay)
 	task.async(function ()
 		claw.down()
 		task.sleep(.5)
 		claw.open()
 	end)
 	
+	if chargedelay then
+		task.sleep(chargedelay)
+	end
 	drive:fd{inches=fddist}
 --	task.sleep(2)
 	claw.down_grab{wait=true}
@@ -41,23 +133,23 @@ end
 
 local sponge_queue = { "medium", "small", "large" }
 
-function drop_sponge(fddist, cursponge)
+function drop_sponge(chargedist, cursponge)
 	print("Dropping " .. cursponge .. " sponge")
 	task.async(sponge.select, cursponge)
 	
 	local side
-	if fddist > 0 then
-		drive:fd{inches=fddist}
+	if chargedist > 0 then
+		drive:fd{inches=chargedist}
 		side = algorithm.drive_to_oilslick("fd")
 	else
-		drive:bk{inches=-fddist}
+		drive:bk{inches=-chargedist}
 		side = algorithm.drive_to_oilslick("bk")
 	end
 	print("Got oil slick on side " .. side)
-	task.sleep(2)
 
 	local turnamt
-	local fddist = 2
+	local fddist = 1
+	fddist = 2
 	if cursponge == "small" then
 		turnamt = -90
 	elseif cursponge == "large" then
@@ -71,9 +163,17 @@ function drop_sponge(fddist, cursponge)
 		else
 			fddist = fddist + 3
 		end
+		
+		if chargedist < 0 then
+			fddist = fddist - 3
+		end
 	else
 		turnamt = 0
 		fddist = fddist + 1
+		
+		if chargedist < 0 then
+			fddist = fddist - 1
+		end
 	end
 	
 	if side == "center-left" then
@@ -90,7 +190,9 @@ function drop_sponge(fddist, cursponge)
 	
 	print("Turnamt", turnamt)
 	print("Fddist", fddist)
-	drive:fd{inches=fddist}
+	if fddist > 0 then
+		drive:fd{inches=fddist}
+	end
 	turn(turnamt)
 	sponge.release()
 	task.async(sponge.reset)
