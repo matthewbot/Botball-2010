8e5d6806e84943757fd654d45ee79dec399b9423
Matt Thompson <matthewbot@gmail.com>
2010-06-30 13:07:12 -0400

    Improved vision test

--- mb/src/tests/vision.lua
+++ mb/src/tests/vision.lua
@@ -1,25 +1,35 @@
 vision = require "mb.vision"
 
 camera = vision.Camera()
-cm = vision.ColorModel{
-	lefthue = 0,
-	righthue = 360,
+cm_green = vision.ColorModel{
+	lefthue = 88,
+	righthue = 156,
 	minsat = 100,
 	minval = 100
 }
-gip = vision.GridImageProcessor(4, 2)
-gip:addColorModel(cm)
+cm_red = vision.ColorModel{
+	lefthue = 336,
+	righthue = 41,
+	minsat = 150,
+	minval = 100	
+}
+gip = vision.GridImageProcessor(8, 5)
+gip:addColorModel(cm_green)
+gip:addColorModel(cm_red)
 
-function vtest()
+function vtest(cm)
+	cm = cm or cm_green
 	local image = camera:readImage()
 	gip:processImage(image)
 	
-	for y=0,1 do
-		local buf = ""
-		for x=0,3 do
-			buf = buf .. gip:getCount(x, y, cm) .. " "
+	for x=0,7 do
+		for y=0,3 do
+			local count = gip:getCount(x, y, cm)
+			if count > 15 then
+				print(x .. "," .. y .. " " .. count)
+			end
 		end
-		print(buf)
 	end
+	print("Done")
 end
 
